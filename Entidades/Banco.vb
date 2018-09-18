cd209e2e1f73fec783e5acc80ba6eee3770d2b01
Fabio Dos Santos <fabio2st@yahoo.com>
2018-09-17 22:41:01 -0300

    Asociación múltiple

--- Entidades/Banco.vb
+++ Entidades/Banco.vb
@@ -1,8 +1,10 @@
 ﻿Public Class Banco
     Private _nombre As String
+    Private _clientes As List(Of Cliente)
 
     Sub New()
         Nombre = ""
+        _clientes = New List(Of Cliente)
     End Sub
 
     Public Property Nombre As String
@@ -13,4 +15,16 @@
             _nombre = value
         End Set
     End Property
+
+    Public Sub addCliente(cliente As Cliente)
+        _clientes.Add(cliente)
+    End Sub
+
+    Public Sub removeCliente(cliente As Cliente)
+        _clientes.Remove(cliente)
+    End Sub
+
+    Public Function GetAllClientes() As List(Of Cliente)
+        Return _clientes
+    End Function
 End Class
---
+++ Tests/BancoTest.vb
@@ -0,0 +1,21 @@
+﻿Imports Entidades
+
+Module BancoTest
+    Sub main()
+        Dim icbsi As New Banco()
+        icbsi.Nombre = "icbsi"
+        icbsi.addCliente(New Cliente("Perez", "Juan"))
+        icbsi.addCliente(New Cliente("Epson", "Pepe"))
+        Dim c As New Cliente("chau", "fue")
+        icbsi.addCliente(c)
+        mostrarTodos(icbsi)
+        icbsi.removeCliente(c)
+        mostrarTodos(icbsi)
+    End Sub
+
+    Private Sub mostrarTodos(icbsi As Banco)
+        For Each cliente In icbsi.GetAllClientes
+            Console.WriteLine(cliente.ToString)
+        Next
+    End Sub
+End Module
